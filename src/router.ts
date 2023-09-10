8ce5e8a559d6dcf47abdc5fb1b359e8c2c997b11
Dondomie Dungca <41827126+nodnod08@users.noreply.github.com>
2023-09-10 19:31:18 +0800

    Update the env

--- src/router.ts
+++ src/router.ts
@@ -10,7 +10,7 @@ export const app = createApp(App);
 app.use(pinia);
 
 export const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory("https://main--capable-moxie-0abfb6.netlify.app/"),
   routes: [
     {
       name: "home",
